fb1533bda8a4396863b56f0d5d92d8ea8994e002
Paul Miller <jettero@cpan.org>
2009-07-03 07:38:50 -0400

    CQ-E

---
+++ CuteQueries/Error.pm
@@ -0,0 +1,102 @@
+package XML::Twigx::Error;
+
+use strict;
+use warnings;
+use Carp;
+use Config;
+use overload '""' => "stringify", fallback=>1;
+use base 'Class::Accessor'; __PACKAGE__->mk_accessors(qw(type text));
+
+use constant IS_SUCCESS     => 1;
+use constant IS_QUERY_ERROR => 2;
+use constant IS_DATA_ERROR  => 3;
+
+my $USEDBY = "???";
+sub import { $USEDBY = caller; return }
+
+# new {{{
+sub new {
+    my $class = shift;
+    my $this = bless {@_}, $class;
+
+    # This is ripped off from IPC::System::Simple::Exception, it's pretty hot
+
+    my ($package, $file, $line, $sub);
+
+    my $depth = 0;
+    while (1) {
+        $depth++;
+        ($package, $file, $line, $sub) = CORE::caller($depth);
+
+        # Skip up the call stack until we find something outside
+        # of the caller, $class or eval space
+
+        next if $package->isa($USEDBY);
+        next if $package->isa($class);
+        next if $package->isa(__PACKAGE__);
+        next if $file =~ /^\(eval\s\d+\)$/;
+
+        last;
+    }
+
+    # We now have everything correct, *except* for our subroutine
+    # name.  If it's __ANON__ or (eval), then we need to keep on
+    # digging deeper into our stack to find the real name.  However we
+    # don't update our other information, since that will be correct
+    # for our current exception.
+
+    my $first_guess_subroutine = $sub;
+    while (defined $sub and $sub =~ /^\(eval\)$|::__ANON__$/) {
+        $depth++;
+        $sub = (CORE::caller($depth))[3];
+    }
+
+    # If we end up falling out the bottom of our stack, then our
+    # __ANON__ guess is the best we can get.  This includes situations
+    # where we were called from the top level of a program.
+
+    if (not defined $sub) {
+        $sub = $first_guess_subroutine;
+    }
+
+    $this->{package}  = $package;
+    $this->{file}     = $file;
+    $this->{line}     = $line;
+    $this->{caller}   = $sub;
+
+    return $this;
+}
+# }}}
+
+sub query_error {
+    my $this = shift;
+
+    return $this->{type} == IS_DATA_ERROR;
+    return;
+}
+
+sub data_error {
+    my $this = shift;
+
+    return $this->{type} == IS_QUERY_ERROR;
+    return;
+}
+
+sub throw {
+    my $this = shift;
+
+    croak $this;
+}
+
+sub stringify {
+    my $this = shift;
+
+    return "... a error dude";
+}
+
+sub is_success {
+    my $this = shift;
+
+    return 1 if $this->{type} == IS_SUCCESS;
+    return;
+}
