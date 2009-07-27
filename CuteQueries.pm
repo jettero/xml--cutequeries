75520604285df1e70fe5f700ff4bab5933459b0c
Paul Miller <jettero@cpan.org>
2009-07-27 07:43:04 -0400

    getting ready for 6600

--- CuteQueries.pm
+++ CuteQueries.pm
@@ -10,7 +10,7 @@ use base 'XML::Twig';
 use constant LIST   => 1;
 use constant KLIST  => 2;
 
-our $VERSION = '0.6502';
+our $VERSION = '0.6600';
 
 our %VALID_OPTS = (map {$_=>1} qw(nostrict nofilter_nontags notrim klist));
 
